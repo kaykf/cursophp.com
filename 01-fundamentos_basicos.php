01ff3917892b9dd87aa45332d6c593324315f1b0
kaykf <kaykfelipe071@gmail.com>
2024-11-07 04:03:59 -0300

    aula 01
    fundamentos basicos do php

---
+++ 01-fundamentos_basicos.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Fundamentos Básicos</title>
+</head>
+<body>
+
+<?php
+
+	// esse é meu primeiro arquivo em php
+	/* Esse é meu primeiro arquivo
+	de suas linhas */
+	
+	echo "meu nome é kayk <br>";
+	print"Minha idade é 19";
+
+?>
+
+</body>
+</html>
