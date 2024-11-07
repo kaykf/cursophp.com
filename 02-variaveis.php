ce13c4552f45166a773f39a51e159baf1149d143
kaykf <kaykfelipe071@gmail.com>
2024-11-07 04:04:32 -0300

    aula02
    variaveis simples em php

---
+++ 02-variaveis.php
@@ -0,0 +1,30 @@
+<?php
+// Meus dados
+$nome = "Kayk Felipe";
+$idade = 19;
+$altura = 1.82;
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura<br>";
+
+//Alterando meus dados
+$nome = "felipe Moraes";
+$idade = 20;
+ $altura = 1.95;
+ echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura";
+ echo "<hr>";
+
+ $carro = "Mustang";
+ echo $carro;
+/*
+ //Nomes válidos
+ $nome
+ $_meunome
+ $carro10
+ $nota1
+ $meu_nome
+ $meuNome
+
+ //Nome inválidos
+ $123carro
+ $carro*@!
+ $meu carro
+ */
